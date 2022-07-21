6789273c572e92bad880329093a219435db8c559
Oluwakemi <adedayo.akerele@fintraksoftware.com>
2022-07-20 20:55:49 -0700

    Added Challenge 2

---
+++ Challenge2/app.js
@@ -0,0 +1,16 @@
+import Auth from './auth.js';
+
+
+const loginBtn = document.getElementById('login_btn');
+const regBtn = document.getElementById('rgstr_btn');
+
+const newAuth = new Auth();
+loginBtn.addEventListener("click", e=>{
+    debugger
+   newAuth.logIn();
+})
+regBtn.addEventListener("click", e=>{
+    debugger
+    newAuth.store();
+   
+})
