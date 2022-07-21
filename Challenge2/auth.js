04e8b063af1b92ece21dc398048c26e4fe24df18
Oluwakemi <adedayo.akerele@fintraksoftware.com>
2022-07-21 03:36:08 -0700

    adjusting module

--- Challenge2/auth.js
+++ Challenge2/auth.js
@@ -73,7 +73,7 @@ export default class Auth {
     logIn(){
         if(userName.value == storedName && userPw.value == storedPw){
             // alert('You are logged in.');
-             return window.location.assign("./home.html");
+             return window.location.assign("home.html");
              //window.location.href = "/yes.html";
             
          }else{
